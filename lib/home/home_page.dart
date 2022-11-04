c5cfaf700a7b78b91a4f91d844cdfe535de10453
Thiago Amorim <thiagoamorim@MacBook-Air-de-Thiago.local>
2022-11-04 16:35:08 -0300

    exclusao de arquivos e package nao utilizados

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,51 +1,40 @@
-import 'package:demo_remote_localization/application/app_localizations/app_localizations_cubit.dart';
 import 'package:demo_remote_localization/main.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppLocalizationsCubit()..getRemoteStrings(),
-      child: BlocConsumer<AppLocalizationsCubit, AppLocalizationsState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                TestLocalization.of(context).appLocalizations.demoRemote,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            TestLocalization.of(context).appLocalizations.demoRemote,
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '${TestLocalization.of(context).appLocalizations.hello}! ${TestLocalization.of(context).appLocalizations.welcome}',
+                style: const TextStyle(fontSize: 28),
               ),
-              centerTitle: true,
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${TestLocalization.of(context).appLocalizations.hello}! ${TestLocalization.of(context).appLocalizations.welcome}' ,
-                    style: const TextStyle(fontSize: 28),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    TestLocalization.of(context).appLocalizations.startDemo,
+                    style: const TextStyle(fontSize: 18),
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 50,
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: () {}, 
-                      child: Text(
-                        TestLocalization.of(context).appLocalizations.startDemo,
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            )
-          );
-        },
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:demo_remote_localization/home/home_page.dart';
 import 'package:demo_remote_localization/infrastructure/app_localizations_repository.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -18,13 +17,10 @@ void main() async {
   await prefs.setString('remoteStrings', jsonEncode(getRemoteStrings));
 
   runApp(
-    RepositoryProvider(
-      create:(context) => AppLocalizationsRepository(),
-      child: MaterialApp(
-        home: TestLocalization(
-          appLocalizations: AppLocalizationsModel.fromJson(getRemoteStrings),
-          child: const HomePage(),
-        ),
+    MaterialApp(
+      home: TestLocalization(
+        appLocalizations: AppLocalizationsModel.fromJson(getRemoteStrings),
+        child: const HomePage(),
       ),
     ),
   );
